ff99bd0568a94c3f7c406731bb2cf595fca6913f
tjcocklin <jcocklin@email.sc.edu>
2019-02-02 17:36:50 -0500

    Added commented code for the eventFail.json

--- cognito_form/tests/test_repromptIntent/test.js
+++ cognito_form/tests/test_repromptIntent/test.js
@@ -1,6 +1,7 @@
 var lambda = require('../../lambda/us-east-1_cognito-skill/index.js');
 var context = require('./context.js');
 var mockEvent = require('./event.json');
+//var mockEvent = require('./eventFail.json');
 
 
 var mockContext = new context();
