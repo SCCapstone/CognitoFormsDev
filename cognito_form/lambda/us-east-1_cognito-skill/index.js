dc54ddca7b033b80015261e7f827ea5fbae24e7e
kblock97 <43151096+kblock97@users.noreply.github.com>
2019-04-03 16:01:52 -0400

    update with helper functions

--- cognito_form/lambda/us-east-1_cognito-skill/index.js
+++ cognito_form/lambda/us-east-1_cognito-skill/index.js
@@ -259,17 +259,46 @@ function ansObject(question, ans, type, subType){
 
 class helperFunctions{
 
-      static getRandomInt(max) {
+    static getRandomInt(max) {
            return Math.floor(Math.random() * Math.floor(max));
+      }  
+  var get = function (obj, path, def) {
+
+	/**
+	 * If the path is a string, convert it to an array
+	 * @param  {String|Array} path The path
+	 * @return {Array}             The path array
+	 */
+	var stringToPath = function (path) {
+        // If the path isn't a string, return it
+        if (typeof path !== 'string') return path;
+        // Create new array
+        var output = [];
+        // Split to an array with dot notation
+        path.split('.').forEach(function (item) {
+          // Split to an array with bracket notation
+          item.split(/\[([^}]+)\]/g).forEach(function (key) {
+            // Push to the new array
+            if (key.length > 0) {
+              output.push(key);
+            }
+          });
+        });
+        return output;
+      };
+      // Get the path as an array
+      path = stringToPath(path);
+      // Cache the current object
+      var current = obj;
+      // For each item in the path, dig into the object
+      for (var i = 0; i < path.length; i++) {
+        // If the item isn't found, return the default (or null)
+        if (!current[path[i]]) return def;
+        // Otherwise, update the current  value
+        current = current[path[i]];
       }
-        
-    static helperGet() {
-        for(var i=0; i<10; i++) {
-            
-        
-        
-        }
-    }
+      return current;
+    };
 }
 // https://services.cognitoforms.com/forms/api/6e238844-ce7a-489a-be61-fdef351fadd4/forms
 const handlers = {
